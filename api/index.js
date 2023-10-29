d7135da16e2b64ed181437bb75053f04a75df5b8
Rone47 <ryanbyrone47@gmail.com>
2023-10-29 22:38:23 +0300

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import  express  from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from  './routes/user.route.js';
+import  authRouter  from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO)
     });
 
 const app = express();
+
+app.use(express.json());
 
 
 
@@ -42,3 +45,4 @@ app.listen(3000, () => {
 });    
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);

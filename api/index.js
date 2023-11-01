d9b7fb737f2148fcd3d1e766746d4c6c8f07f6d8
Rone47 <ryanbyrone47@gmail.com>
2023-11-01 22:44:15 +0300

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from  './routes/user.route.js';
 import  authRouter  from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -53,14 +53,18 @@ export default function Profile() {
       <h1 className='text-3xl font-semibold text-center m-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
       <input onChange={(e)=>setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*' />
-        <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+        <img onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
         <p className='text-sm self-center'>
           {fileUploadError ? (
-            <span className='text-red-700 font-medium'>Error Image Upload (image must be less than 2mbs)</span>
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
           ) : filePerc > 0 && filePerc < 100 ? (
-            <span className='text-slate-700 font-medium'>{`Uploading ${filePerc}%`}</span>
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
           ) : filePerc === 100 ? (
-            <span className='text-green-700 font-medium'>Image successfully uploaded!</span>
+            <span className='text-green-700'>Image successfully uploaded!</span>
           ) : (
             ''
           )}

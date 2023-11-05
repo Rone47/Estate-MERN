25fefe07e906de91a0a2c0a97a032f950a9ecc77
Rone47 <ryanbyrone47@gmail.com>
2023-11-05 20:46:46 +0300

    add signOut functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import {useSelector} from 'react-redux';
 import {useRef, useState, useEffect} from 'react';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart } from '../redux/user/userSlice';
 import  {useDispatch} from 'react-redux';
 
 
@@ -97,6 +97,21 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch ('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
   }
   return (
     <div className='p-3 max-w-xl mx-auto'>
@@ -169,11 +184,11 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer font-medium'>Delete acoount</span>
-        <span className='text-red-700 cursor-pointer font-medium'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer font-medium'>Sign out</span>
       </div>
 
       <p className='text-red-700 mt-5'>{error ? error: ''}</p>
-      <p className='text-green-700 mt-5'>{updateSucces ?'User is updated successfully!' : ''}</p>
+      <p className='text-green-700 mt-5'>{updateSucces ? 'User is updated successfully!' : ''}</p>
     </div>
   )
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -45,7 +45,20 @@ const userSlice = createSlice({
      deleteUserFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
+     },
+     signOutUserStart: (state) => {
+      state.loading = true;
+     },
+     signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+     },
+     signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
      }
+
     }
 });
 
@@ -58,7 +71,10 @@ export const {
     updateUserSuccess,
     deleteUserSuccess,
     deleteUserStart,
-    deleteUserFailure
+    deleteUserFailure,
+    signOutUserFailure,
+    signOutUserSuccess,
+    signOutUserStart
 } = userSlice.actions;
 
 export default userSlice.reducer;

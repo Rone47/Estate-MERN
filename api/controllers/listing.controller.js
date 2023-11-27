b66ff5c1cdeefa70f1e26f6d65b866555ff510fa
Rone47 <ryanbyrone47@gmail.com>
2023-11-27 19:51:08 +0300

    update

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,65 +1,63 @@
-import Listing from "../models/listing.model.js";
+import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
-
 export const createListing = async (req, res, next) => {
-   
-    try {
-        const listing =await Listing.create(req.body);
-        return res.status(201).json(listing);
-    } catch (error) {
-        next(error);
-    }
-
-
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteListing = async (req, res, next) => {
-    const listing = await Listing.findById(req.params.id);
-    if (!listing) {
-        return next(errorHandler(404 , "Listing not found"));
-    } 
+  const listing = await Listing.findById(req.params.id);
 
-    if (req.user.id !== listing.userRef){
-        return next (errorHandler(404 , "You can only delete your own listings!"));
-    }
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
 
-    try {
-        await Listing.findIdByDelete(req.params.id);
-        res.status(200).json('Listing has been deleted');
-    } catch (error) {
-        next(error);
-    }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only delete your own listings!'));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json('Listing has been deleted!');
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateListing = async (req, res, next) => {
-    const listing = await Listing.findById(req.params.id);
-    if (!listing) {
-        return next(errorHandler(404 , "Listing not found"));
-    }
-    if(req.user.id !== listing.userRef) {
-        return next(errorHandler(401, "You can only update your own listings!"));
-    }
-    try {
-        const updateListing = await Listing.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {new:true}
-        );
-        request.status(200).json(updatedListing);
-    } catch (error) {
-        next(error)
-    }
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getListing = async (req, res, next) => {
-    try {
-        const listing = await Listing.findById(req.params.id);
-        if (!listing) {
-            return next(errorHandler(404, "Listing not found"));
-        }
-        res.status(200).json(listing);
-    } catch (error) {
-        next(error)
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
     }
-}
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,23 +1,33 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore from 'swiper';
+import {Navigation} from 'swiper/modules';
+import 'swiper/css/bundle';
+
 
 export default function Listing() {
+  SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const params = useParams();
   useEffect(() =>{
     const fetchListing = async () => {
       try {
         setLoading(true);
-        const res = await fetch (`/api/listing/get/${params.listingId}`);
-        const data = await res.json();
-        if(data.succes === false) {
+        console.log('Listing ID:', params.listingId);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+       
+        const data = await res.json(); 
+        if(data.success === false) {
           setError(true);
           setLoading(false);
           return;
         }
         setListing(data);
+        setLoading(false);
+        setError(false);
       } catch (error) {
         setError(true);
         setLoading(false);
@@ -25,10 +35,15 @@ export default function Listing() {
 
     }
     fetchListing();
-  })
+  }, [params.listingId]);
+
   return (
-    <div>
-      {listing && listing.name}
-    </div>
+    <main>
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+      {error && <p className='text-center my-7 text-2xl'>Something went wrong!</p>}
+      {listing && !loading && !error (
+        <h1>{listing.name}</h1>
+      )}
+    </main>
   )
 }

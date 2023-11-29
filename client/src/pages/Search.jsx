612a2f6c12c962b28e837a3c39811e249588ccd4
Rone47 <ryanbyrone47@gmail.com>
2023-11-29 23:15:02 +0300

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -252,7 +252,7 @@ export default function Search() {
           {showMore && (
             <button
               onClick={onShowMoreClick}
-              className='text-green-700 hover:underline p-7 text-center w-full'
+              className='text-green-700 hover:underline p-7 text-center w-full font-semibold'
             >
               Show more
             </button>

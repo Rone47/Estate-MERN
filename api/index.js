105725899592bd93715d436630b4141420ba4eab
Rone47 <ryanbyrone47@gmail.com>
2023-11-30 23:20:01 +0300

    Deploy codes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from  './routes/user.route.js';
 import  authRouter  from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from "cookie-parser";
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO)
     }).catch((err) => {
         console.log(err);
     });
+
+    const _dirname = path.resolve();
 
 const app = express();
 
@@ -33,6 +36,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;

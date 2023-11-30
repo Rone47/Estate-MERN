ea34a2dc0720c27389242027deeccc3d6eb2a3ed
Rone47 <ryanbyrone47@gmail.com>
2023-11-30 22:12:11 +0300

    update redux toolkit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-    About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+      Real Estate
+    </h1>
+    <p className='mb-4 text-slate-700  font-semibold'>Welcome to our Real Estate, where we turn dreams of finding the perfect home into reality. Established with a commitment to excellence, Real Estate is your trusted partner in the journey of buying, selling, or renting properties.</p>
+    <p className='mb-4 text-slate-700 font-semibold'>Our mission is to redefine the real estate experience. We strive to create a seamless and stress-free process for our clients, ensuring that every transaction, whether big or small, is handled with utmost professionalism and care.</p>
+    <p className='mb-4 text-slate-700 font-semibold'>Explore the possibilities with our Real Estate. Contact us today to start your journey toward finding the home of your dreams or achieving your real estate goals. We're here for you every step of the way.</p>
+
     </div>
   )
 }
